fa81196634428c209fd5c8f949f0309c216b94ee
Monis Yousuf <monisyousuf@MacBookPro.fritz.box>
2024-10-05 12:40:34 +0200

    CD-011: Docker on Production

---
+++ CD_011_docker_production/src/test/java/com/my/docker/prod/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.docker.prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
